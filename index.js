c2ea75c4fd4f14528cbc36ad9b3864f6c496e5e1
Matilda <ogarimatilda@gmail.com>
2024-03-27 14:02:41 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+
+const num1 = 31
+const num2 = 2
+
+let multiply = num1 * num2
+console.log(multiply)
+
+let random = Math.floor(Math.random()* 10)
+Number.isInteger(random)
+console.log(random)
+
+const num3 = 4
+const num4 = 7
+let mod = num3 % num4
+console.log(mod)
+
+
+const max = Math.max(15, 10, 9, 20, 1, 9)
+console.log(max)
